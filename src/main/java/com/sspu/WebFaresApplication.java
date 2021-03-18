f4b4d2ede6f8a0002ffbfdab24e35b45d55a90f4
zhanglu1234 <1240161843@qq.com>
2021-03-19 00:10:43 +0800

    Initial commit

---
+++ src/main/java/com/sspu/WebFaresApplication.java
@@ -0,0 +1,13 @@
+package com.sspu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFaresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFaresApplication.class, args);
+    }
+
+}
